9a636b0ad0ada7da80efd190cddf1c165dbaff88
JoseCarlos056 <josecarlos.depaula17@gmail.com>
2020-12-24 09:02:30 -0300

    First

---
+++ src/schemas/User.ts
@@ -0,0 +1,19 @@
+import { Schema, model, Document } from 'mongoose'
+interface IUserSchema extends Document{
+  email?: string
+  firstName?: string
+  lastName?: string
+  fullName(): string
+}
+
+const UserSchema = new Schema({
+  email: String,
+  firstName: String,
+  lastName: String
+}, {
+  timestamps: true
+})
+UserSchema.methods.fullName = function () : string {
+  return this.firstName + ' ' + this.lastName
+}
+export default model<IUserSchema>('User', UserSchema)
